1347c0559c00372758d1abad29a8a46a7fcf93ef
kevaughn <kevaughn@steelhouse.com>
2021-05-10 14:06:23 -0700

    track membership status

---
+++ src/main/java/com/steelhouse/membership/model/Membership.java
@@ -0,0 +1,87 @@
+package com.steelhouse.membership.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import org.springframework.data.annotation.Id;
+
+
+@DynamoDBTable(tableName = "memberships")
+public class Membership {
+
+    @Id
+    private com.steelhouse.membership.model.MembershipKey membershipKey;
+
+    private String source;
+    private Long epoch;
+    private Integer aid;
+
+    @DynamoDBHashKey(attributeName = "ip")
+    public String getIp() {
+        return membershipKey.getIp();
+    }
+
+    public void setIp(String ip) {
+        if (membershipKey == null) {
+            membershipKey = new MembershipKey();
+        }
+        this.membershipKey.setIp(ip);
+    }
+
+    @DynamoDBRangeKey(attributeName = "segment")
+    public String getSegment() {
+        return membershipKey.getSegment();
+    }
+
+    public void setSegment(String segment) {
+        if (membershipKey == null) {
+            membershipKey = new MembershipKey();
+        }
+        this.membershipKey.setSegment(segment);
+    }
+
+    @DynamoDBAttribute(attributeName = "source")
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @DynamoDBAttribute(attributeName = "epoch")
+    public Long getEpoch() {
+        return epoch;
+    }
+
+    public void setEpoch(Long epoch) {
+        this.epoch = epoch;
+    }
+
+    @DynamoDBAttribute(attributeName = "aid")
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        com.steelhouse.membership.model.Membership that = (com.steelhouse.membership.model.Membership) o;
+        return java.util.Objects.equals(membershipKey, that.membershipKey) &&
+                java.util.Objects.equals(source, that.source) &&
+                java.util.Objects.equals(epoch, that.epoch) &&
+                java.util.Objects.equals(aid, that.aid);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(membershipKey, source, epoch, aid);
+    }
+}
+
--- src/main/kotlin/com/steelhouse/membership/controller/ThirdPartyConsumer.kt
+++ src/main/kotlin/com/steelhouse/membership/controller/ThirdPartyConsumer.kt
@@ -1,9 +1,12 @@
 package com.steelhouse.membership.controller
 
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.steelhouse.core.model.gsonmessages.GsonMessageUtil
 import com.steelhouse.core.model.segmentation.gson.MembershipUpdateMessage
 import com.steelhouse.membership.configuration.RedisConfig
+import com.steelhouse.membership.model.Membership
 import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
 import io.micrometer.core.instrument.MeterRegistry
 import kotlinx.coroutines.CoroutineScope
@@ -22,15 +25,18 @@ class ThirdPartyConsumer constructor(@Qualifier("app") private val log: Log,
                                      private val meterRegistry: MeterRegistry,
                                      @Qualifier("redisConnectionPartner") private val redisConnectionPartner: StatefulRedisClusterConnection<String, String>,
                                      @Qualifier("redisConnectionMembership") private val redisConnectionMembership: StatefulRedisClusterConnection<String, String>,
+                                     private val amazonDynamoDB: AmazonDynamoDB,
                                      private val redisConfig: RedisConfig): BaseConsumer(log = log,
         meterRegistry = meterRegistry, redisConnectionPartner = redisConnectionPartner,
         redisConnectionMembership = redisConnectionMembership,
         redisConfig = redisConfig) {
 
+    private val mapper = DynamoDBMapper(amazonDynamoDB)
+
     @KafkaListener(topics = ["sh-dw-generated-audiences"], autoStartup = "\${membership.oracleConsumer:false}")
     @Throws(IOException::class)
     override fun consume(message: String) {
-
+        log.error(message)
         val oracleMembership = GsonMessageUtil.deserialize(message, MembershipUpdateMessage::class.java)
 
         lock.acquire()
@@ -38,18 +44,84 @@ class ThirdPartyConsumer constructor(@Qualifier("app") private val log: Log,
         CoroutineScope(context).launch {
             try {
 
-                val segments = oracleMembership.currentSegments.stream().map { it.toString() }.collect(Collectors.joining(","))
+                val membershipEvent = async {
+                    processMembershipEvents(oracleMembership)
+                }
 
                 val membershipResult = async {
-                    writeMemberships(oracleMembership.ip.orEmpty(), segments, oracleMembership.aid.toString(), "ip", Audiencetype.oracle.name)
+
+                    val segments = oracleMembership.currentSegments.stream().map { it.toString() }
+                            .collect(Collectors.joining(","))
+
+                    writeMemberships(oracleMembership.ip.orEmpty(), segments,
+                            oracleMembership.aid.toString(), "ip", Audiencetype.oracle.name)
                 }
 
                 membershipResult.await()
+                membershipEvent.await()
             } finally {
                 lock.release()
             }
         }
         }
+
+    private suspend fun processMembershipEvents(oracleMembership: MembershipUpdateMessage) {
+
+        CoroutineScope(context).launch {
+
+           val currentEvents =  async {
+                val events = generatePayload(oracleMembership, oracleMembership.currentSegments)
+                save(events)
+            }
+
+            val oldEvents = async {
+                val oldEvents = generatePayload(oracleMembership, oracleMembership.oldSegments)
+                delete(oldEvents)
+            }
+
+            currentEvents.await()
+            oldEvents.await()
+        }
+
+    }
+
+    private fun generatePayload(oracleMembership: MembershipUpdateMessage, segments: List<Int>): Set<Membership> {
+        val events = mutableSetOf<Membership>()
+
+        for (segment in segments) {
+            val membership = Membership()
+
+            membership.ip = oracleMembership.ip
+            membership.segment = segment.toString()
+            membership.source = oracleMembership.source
+            membership.epoch = oracleMembership.epoch / 1000
+            membership.aid = oracleMembership.aid
+
+            events.add(membership)
+        }
+
+        return events
+    }
+
+    private fun delete(data: Set<Membership>) {
+
+        val results = mapper.batchDelete(data)
+
+        for (x in results) {
+            log.error(x.exception.toString())
+            log.error(x.unprocessedItems.size.toString() + " events failed to process")
+        }
+    }
+
+    private fun save(data: Set<Membership>) {
+
+            val results = mapper.batchSave(data)
+
+            for (x in results) {
+                log.error(x.exception.toString())
+                log.error(x.unprocessedItems.size.toString() + " events failed to process")
+            }
+    }
 
 
 }
