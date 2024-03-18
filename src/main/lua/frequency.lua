501955cdaa15287ef1b72b7196030b9b3367abf9
kevaughn bullock <bullock82@gmail.com>
2024-03-18 14:38:42 -0700

    Dev (#46)
    * Update the F cap lua script to support storing the list of impression… (#45)
    
    * Update the F cap lua script to support storing the list of impression epochs as a comma-separated String. This will improve the bidder's read performance for keys with many impressions.

--- src/main/lua/frequency.lua
+++ src/main/lua/frequency.lua
@@ -11,6 +11,7 @@ redis.call("HSET", key, tdImpressionId, impressionEpoch)
 
 -- trim excess epochs and expired
 local impressionIdEpochs = redis.call("HGETALL", key)
+local validImpressionEpochs = {}
 
 for i = 1, #impressionIdEpochs, 2 do
     local recordKey = impressionIdEpochs[i]
@@ -18,8 +19,20 @@ for i = 1, #impressionIdEpochs, 2 do
 
     if tonumber(recordValue) < epochExpirationLimit then
         redis.call("HDEL", key, recordKey)
+    else
+        validImpressionEpochs[#validImpressionEpochs+1] = recordValue
     end
 end
 
 -- set expire on deviceId
 redis.call("EXPIRE", key, deviceTTL)
+
+-- compress map of epochs into a comma separated String
+local hashedKey = "{" .. key .. "}" .. "_epochs"
+if next(validImpressionEpochs) == nil then
+    redis.call("DEL", hashedKey)
+else
+    redis.call("SET", hashedKey,  table.concat(validImpressionEpochs, ","), "EX", deviceTTL)
+end
+
+
