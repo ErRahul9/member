31470b198c90e5843b253e4a46e1b92961d1926d
changxingc <changxing@mountain.com>
2024-05-14 16:20:38 -0500

    add full migration for all services

--- src/main/kotlin/com/steelhouse/membership/Application.kt
+++ src/main/kotlin/com/steelhouse/membership/Application.kt
@@ -1,17 +1,11 @@
 package com.steelhouse.membership
 
-import org.apache.kafka.common.config.SaslConfigs
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
-import org.springframework.context.ApplicationListener
-import org.springframework.context.event.ContextRefreshedEvent
-import org.springframework.core.env.Environment
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.stereotype.Component
 
 
 @EnableScheduling
