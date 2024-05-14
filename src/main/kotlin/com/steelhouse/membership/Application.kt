665005d5bb0a930814a6c028c9d7d6db3f8e5323
changxingc <changxing@mountain.com>
2024-05-13 19:00:14 -0500

    more logs;

--- src/main/kotlin/com/steelhouse/membership/Application.kt
+++ src/main/kotlin/com/steelhouse/membership/Application.kt
@@ -1,11 +1,13 @@
 package com.steelhouse.membership
 
 import org.apache.kafka.common.config.SaslConfigs
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.core.env.Environment
 import org.springframework.scheduling.annotation.EnableScheduling
 
 
@@ -25,7 +27,11 @@ open class Application {
             println("Username: " + System.getenv("USERNAME"))
             println("Password: " + System.getenv("PASSWORD"))
             val jaasConfig = System.getProperty(SaslConfigs.SASL_JAAS_CONFIG)
-            println("Current JAAS Config: " + jaasConfig);
+            val jassMec = System.getProperty(SaslConfigs.SASL_MECHANISM)
+
+//            println("Current JAAS Config: " + jaasConfig);
+            println("Current JAAS Config: $jaasConfig")
+            println("Current JAAS Mechanism: $jassMec")
             SpringApplication.run(Application::class.java, *args)
         }
     }
