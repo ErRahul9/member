fa45d92296d0539ac560bfbeef341b510b51a39a
changxingc <changxing@mountain.com>
2024-07-31 17:58:15 -0500

    NOBUG: update gradle git

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
-    id("com.gorylenko.gradle-git-properties") version "2.2.2"
+    id("com.gorylenko.gradle-git-properties") version "2.3.2"
     kotlin("plugin.serialization") version "1.6.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     jacoco
